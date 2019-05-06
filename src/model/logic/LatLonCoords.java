1d7eff3e2f4807a36ac41332d6ae9331184f63e9
Cohnan <sc.puerto10@uniandes.edu.co>
2019-05-06 13:38:04 -0500

    Correccion de distancia por error de grados en lugar dr adianes

--- src/model/logic/LatLonCoords.java
+++ src/model/logic/LatLonCoords.java
@@ -18,9 +18,11 @@ public class LatLonCoords {
 	public double getLon() {return lon;}
 	
 	public double haversineD(LatLonCoords coords2) {
-		double lat2 = coords2.getLat();
-		double lon2 = coords2.getLon();
-		return 2*EARTHS_MEAN_R * Math.asin( Math.sqrt( Math.pow(Math.sin((lat2 - lat)/2), 2) + Math.cos(lat)*Math.cos(lat2)* Math.pow(Math.sin((lon2 - lon)/2), 2) ) );
+		double lat1rad = lat * Math.PI / 180.;
+		double lon1rad = lon * Math.PI / 180.;
+		double lat2rad = coords2.getLat()  * Math.PI / 180.;
+		double lon2rad = coords2.getLon()  * Math.PI / 180.;
+		return 2*EARTHS_MEAN_R * Math.asin( Math.sqrt( Math.pow(Math.sin((lat2rad - lat1rad)/2 ), 2) + Math.cos(lat1rad)*Math.cos(lat2rad)* Math.pow(Math.sin((lon2rad - lon1rad)/2), 2) ) );
 	}
 	
 }
